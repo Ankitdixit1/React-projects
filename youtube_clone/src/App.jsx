280b094eb15dc53338c9cd552bc87222179d6ec0
ankitdixit <ankitdixit29269@gmail.com>
2023-09-18 22:38:31 +0530

    pushing some code

--- youtube_clone/src/App.jsx
+++ youtube_clone/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react"
 import { featchdatafromapi } from "./utils/api"
-
+import { AppContext } from "./context/contextApi"
 function App() {
-  useEffect( ()=>{
-    apitesting();
-  },[]);
-  const apitesting=(query)=>{
-    featchdatafromapi(`search/?q=${query}`).then((res)=>{
-      console.log(res);
-    })
-  }
+  // useEffect( ()=>{
+  //   apitesting();
+  // },[]);
+  // const apitesting=(query)=>{
+  //   featchdatafromapi(`search/?q=${query}`).then((res)=>{
+  //     console.log(res);
+  //   })
+  // }
 
   return (
     <>
+    <AppContext><div>App</div></AppContext>
     <h1 className="text-red-700 text-5xl">
       Hello world!
     </h1>
--- youtube_clone/src/context/contextApi.jsx
+++ youtube_clone/src/context/contextApi.jsx
@@ -2,15 +2,15 @@ import { createContext, useState, useEffect } from "react";
 import {featchdatafromapi} from '../utils/api'
 
 export const Context =  createContext();
-export const AppContext = ()=>{
+export const AppContext = (props)=>{
  const [loading, setloading]=useState(false);
  const [searchResult,setsearchResults] = useState(false);
  const [selectCategories,setselectCategories] = useState("new");
  const [mobilemenu,setmobilemenu] = useState(false);
 
  useEffect(()=>{
-
- },[])
+    fetchselectedcategories(selectCategories);
+ },[selectCategories]);
 
  const fetchselectedcategories= (query)=>{
     featchdatafromapi(`search/?q=${query}`).then((res)=>{
@@ -19,5 +19,12 @@ export const AppContext = ()=>{
         setloading(false);
       })
  }
-    return 
+    return (<Context.Provider
+    value={{loading, setloading,
+        searchResult,setsearchResults,
+        selectCategories,setselectCategories,
+        mobilemenu,setmobilemenu}}
+    >
+        {props.children}
+    </Context.Provider>)
 }
